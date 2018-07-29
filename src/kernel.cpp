94bb7d612b1b9c6f8c80f8325844618bf7b57cf6
TianDaGe <tiandage719@outlook.com>
2018-07-29 04:53:33 +0000

    update GetKernelStakeModifier

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -264,9 +264,11 @@ bool GetKernelStakeModifier(uint256 hashBlockFrom, uint64_t& nStakeModifier, int
 
         pindex = pindexNext;
         pindexNext = chainActive[pindexNext->nHeight + 1];
-        if (pindex->GeneratedStakeModifier()) {
+        // if (pindex->GeneratedStakeModifier()) {
+        if (true) {
             nStakeModifierHeight = pindex->nHeight;
             nStakeModifierTime = pindex->GetBlockTime();
+            break;
         }
     }
     nStakeModifier = pindex->nStakeModifier;
